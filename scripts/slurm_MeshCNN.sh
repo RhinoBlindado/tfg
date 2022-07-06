c7f1f021075f3e4a747fd101db67cc76f5ef46ab
Valentino Lugli valentinolugli@correo.ugr.es - Resp. Pablo Mesejo <vlugli@ngpu>
2022-07-06 20:09:43 +0200

    Edited version of script

--- scripts/slurm_MeshCNN.sh
+++ scripts/slurm_MeshCNN.sh
@@ -27,9 +27,9 @@ conda activate /mnt/homeGPU/vlugli/condaEnvs/meshcnnplus
 trainPath=("./networks/MeshCNNPlus/development/meshcnn/train.py")
 testPath=("./networks/MeshCNNPlus/development/meshcnn/test.py")
 dataPath=("./data/datasets/Nodule-98-30K")
-testName=("Nodule-98-30K-5rev")
+testName=("Nodule-98-30K-7rev")
 
-basicParams=("${trainPath} --csv --dataroot ${dataPath} --name ${testName}  --seed 16")
+basicParams=("${trainPath} --csv --dataroot ${dataPath} --name ${testName} --num_threads 2  --seed 16 --ninput_edges 30000")
 basicTrainParams=("--print_freq 9999 --run_test_freq 9999 --validation --verbose_train")
 
 # Network architecture
@@ -43,31 +43,31 @@ epochsWithLRDecay=("10")
 
 conv=("32 64 256 256")
 pool=("20000 15000 10000 5000")
-resBlocks=("3")
+resBlocks=("0")
 normalization=("batch")
 
 dense=("2042 512 128")
 dropout=("0.25 0.25 0.25")
 
-netArch=("--amsgrad --arch mconvnet --batch_size ${batchSize} --dropout ${dropout} --fc_n ${dense} --gpu_ids 0 --ncf ${conv} --niter ${epochs} --niter_decay ${epochsWithLRDecay} --norm ${normalization} --optimizer ${optimizer} --pool_res ${pool} --resblocks ${resBlocks}")
+netArch=("--amsgrad --arch mconvnet --batch_size ${batchSize} --dropout ${dropout} --fc_n ${dense} --gpu_ids 0 --ncf ${conv} --niter ${epochs} --niter_decay ${epochsWithLRDecay} --norm ${normalization} --num_groups 1  --optimizer ${optimizer} --pool_res ${pool} --resblocks ${resBlocks}")
 
 # Data augmentation
 ###################
 
-edgeFlip=("0.2")
-numAug=("20")
-slideVerts=("0.2")
+edgeFlip=("0")
+numAug=("10")
+slideVerts=("0")
 
 dataAug=("--flip_edges ${edgeFlip} --num_aug ${numAug} --slide_verts ${slideVerts}")
 
 # Training
 ##########
 
-python $basicParams $basicTrainParams $netArch #$dataAug
+python $basicParams $basicTrainParams $netArch $dataAug
 
 # Test
 ######
-python $testPath $basicParams $netArch --verbose --confusion_matrix
+python $testPath $basicParams $netArch --verbose --confusion_matrix --export_folder meshes 
 
 # Avisar por correo de la finalización
-mail -s "Entrenamiento ${testName} finalizado" valentinolugli@correo.ugr.es <<< "Hora finalización: $(date)"
+#mail -s "Entrenamiento ${testName} finalizado" valentinolugli@correo.ugr.es <<< "Hora finalización: $(date)"
