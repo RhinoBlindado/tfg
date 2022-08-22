d196d970a6fbeb44467814fa0c2a60b6ffe00346
Valentino Lugli <valentinolugli@gmail.com>
2022-08-22 16:27:02 +0200

    Starting to clean up scripts

--- scripts/slurm_Nodule-50-30K-7.sh
+++ scripts/slurm_Nodule-50-30K-7.sh
@@ -1,49 +1,74 @@
 #!/bin/bash
 
-# Ordenes para el Gestor de carga de trabajo:
-# 1. Asigna al trabajo un nombre
-#SBATCH --job-name=Mr_Bones_Wild_Ride
+# Asigna al trabajo un nombre
+#SBATCH --job-name=N-50-30K-7
 
-# 2. Asignar el trabajo a una partición (cola)
+# Asignar el trabajo a una partición (cola)
 #SBATCH --partition=dios
 
-# 3. Asignar el trabajo a un GPU
-##SBATCH --nodes=2
-##SBATCH --ntasks-per-node=1
+# Asignar el trabajo a un GPU
 #SBATCH --gres=gpu:1
-##SBATCH --mem=16GB
 
-# 4. Asignar el trabajo a un nodo en particular.
-##SBATCH --nodelist=dionisio
+# Asignar el trabajo a un nodo en particular.
+#SBATCH --nodelist=dionisio
 
-# Configuraciones por defecto de NGPU:
+# Configuraciones por defecto de NGPU
 export PATH="/opt/anaconda/anaconda3/bin:$PATH"
 export PATH="/opt/anaconda/bin:$PATH"
 eval "$(conda shell.bash hook)"
 export TFHUB_CACHE_DIR=.
 
-# Activando el enviroment de Conda:
+# Activating conda enviroment
 conda activate /mnt/homeGPU/vlugli/condaEnvs/meshcnnplus
 
-python ./networks/MeshCNNPlus/development/train.py \
---dataroot ./data/datasets/Nodule-50-30K \
---name Nodule-50-30K-7 \
---ninput_edges 30000 \
---ncf 4 8 64 128  256  \
---pool_res 20000 15000 13000 8000 7500 \
---norm group \
---num_groups 1 \
---res_blocks 0 \
---fc_n 1024 512 \
---flip_edges 0.2 \
---slide_verts 0.2 \
---num_aug 2 \
---niter 100 \
---niter_decay 100 \
---num_threads 2 \
---batch_size 1 \
---gpu_ids 0 \
---plus True \
+# Basic parameters
+##################
 
-# Correo para avisarme:
-# mail -s "Proceso finalizado" valentinolugli@correo.ugr.es <<< "El proceso ha finalizado"
+trainPath=("./networks/MeshCNNPlus/development/meshcnn/train.py")
+testPath=("./networks/MeshCNNPlus/development/meshcnn/test.py")
+dataPath=("./data/datasets/Nodule-50-30K")
+testName=("Nodule-50-30K-7")
+
+basicParams=("${trainPath} --csv --dataroot ${dataPath} --name ${testName} --num_threads 2  --seed 16 --ninput_edges 30000")
+basicTrainParams=("--print_freq 9999 --run_test_freq 9999 --validation --verbose_train")
+
+# Network architecture
+######################
+
+optimizer=("adam")
+
+batchSize=("1")
+epochs=("50")
+epochsWithLRDecay=("10")
+
+conv=("32 64 256 256")
+pool=("20000 15000 10000 5000")
+resBlocks=("0")
+normalization=("batch")
+
+dense=("2042 512 128")
+dropout=("0.5 0.5 0.5")
+
+netArch=("--amsgrad --arch mconvnet --batch_size ${batchSize} --dropout ${dropout} --fc_n ${dense} --gpu_ids 0 --ncf ${conv} --niter ${epochs} --niter_decay ${epochsWithLRDecay} --norm ${normalization} --num_groups 1  --optimizer ${optimizer} --pool_res ${pool} --resblocks ${resBlocks}")
+
+# Data augmentation
+###################
+
+edgeFlip=("0.2")
+numAug=("2")
+slideVerts=("0.2")
+
+dataAug=("--flip_edges ${edgeFlip} --num_aug ${numAug} --slide_verts ${slideVerts}")
+
+# Training
+##########
+
+python $basicParams $basicTrainParams $netArch $dataAug
+
+# Test
+######
+python $testPath $basicParams $netArch --verbose --confusion_matrix --export_folder meshes
+
+# Send mail notification
+# - No longer working.
+#mail -s "Entrenamiento ${testName} finalizado" valentinolugli@correo.ugr.es <<< "Hora finalización: $(date)"
